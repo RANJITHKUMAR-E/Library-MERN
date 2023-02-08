78e3de3ce73d63b17b03948bdd3cbbedd570fd0b
Mukilan-T <mukilanthangadurai@gmail.com>
2023-02-08 18:41:32 +0530

    Home_updated

--- client/src/components/HomeView.jsx
+++ client/src/components/HomeView.jsx
@@ -1,38 +1,51 @@
 import styled from "styled-components"
 
 const Container=styled.div`
-    width : 100vw;
-    height : 100vh;
-    padding:28px;
-    background: 
-    url("https://iili.io/HEC6cZB.png") center;
-    background-size: cover;
-    display : flex;
-    align-items : center;
-    justify-content: center;
-    overflow:hidden;
+        
+        height : 100vh;
+        padding:28px;
+        background: 
+        url("https://iili.io/HEC6cZB.png") center;
+        background-size: cover;
+        display : flex;
+        align-items : center;
+        justify-content: center;
 
 `;
 
 const TopWrapper=styled.div`
-        width: 50%;
-        height: 2cm;
+        width: 65%;
+        height: 20px;
         padding: 20px;
-        margin-top:2px;
-        background-color: rgba(240, 228, 228, 0.83);
+        background: 
+        url("https://iili.io/HEnpLUQ.png") left ;
+        position:absolute;
+        background-repeat : no-repeat;
+        margin-top: -490px;
+        background-color: rgba(180, 230, 252, 1);
+        align-items : center;
+        justify-content: center;
+`;
+
+const TopText=styled.div`
+        padding:2px;
+        margin-left:250px;
+        font-size: 20px;
+        font-weight: bold;
 `;
 
 
 const LWrapper=styled.div`
         width: 20%;
-        height: 7cm;
+        height: 8cm;
         padding: 20px;
+        background:url ("https://iili.io/HEnpLUQ.png") center;
         background-color: rgba(240, 228, 228, 0.83);
 `;
 
 const RWrapper=styled.div`
         width: 20%;
-        height: 7cm;
+        height: 8cm;
         padding: 20px;
         margin-left:4cm;
         background-color: rgba(240, 228, 228, 0.83);
@@ -71,21 +84,25 @@ const Button=styled.button`
 `;
 
 
-const Login = () => {
-    return (
+const Home = () => {
+     return (
         <Container>
-            <TopWrapper/>
+        <TopWrapper>
+                <TopText>
+                ONLINE LIBRARY MANAGEMENT SYSTEM
+                </TopText>
+        </TopWrapper>
         <LWrapper>
-            <Admin/>
+        <Admin/>
                 <Button>Admin Login</Button>
         </LWrapper>
 
         <RWrapper>
-            <User/>
+        <User/>
                 <Button>Student Login</Button>
         </RWrapper>
         </Container>
-    )
+)
 }
 
-export default Login
+export default Home
