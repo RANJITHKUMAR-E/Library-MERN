2739980f9caff42e1e804aacc710f3f8d2922bec
RANJITHKUMAR-E <ranjithkumarstudies@gmail.com>
2023-02-09 18:30:26 +0530

    User Controller Completed

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,7 @@
 const userModel=require("../models/userModel");
+const jwt=require('jsonwebtoken')
+const bcrypt=require("bcryptjs")
+const db=require("../db");
 
 exports.getAllUser=async (req, res) =>
 {
@@ -20,8 +23,8 @@ exports.getAllUser=async (req, res) =>
 exports.getUser=async (req, res) =>
 {
     try {
-        const id=req.params.id
-        const user=await userModel.find({_id: id})
+        const user=await userModel.find({rollNo: req.params.id})
+
         res.status(200).json({
             status: "success",
             data: user
@@ -35,17 +38,143 @@ exports.getUser=async (req, res) =>
     }
 }
 
-exports.createUser=(req, res) =>
+
+// Only for admin
+exports.createUser=async (req, res) =>
 {
-    res.status(200).json({status: "Create user Done"});
+    const data=req.body;
+    try {
+        data.password=await bcrypt.hash(req.body.password, 12);
+        const newUser=new userModel(data);
+        await newUser.save();
+
+        const token=jwt.sign({rollNo: req.body.rollNo}, process.env.JWT_KEY, {expiresIn: process.env.JWT_EXPIRES})
+        res.status(200).json({
+            message: "User Created successfully 😌",
+            token,
+            data: data,
+        });
+    } catch(error) {
+        console.log(error);
+        res.status(400).json({
+            status: "Failed",
+            message: error
+        });
+    }
 }
 
-exports.updateUser=(req, res) =>
+exports.updateUser=async (req, res) =>
 {
-    res.status(200).json({status: "Update user Done"});
+    const data=req.body;
+    try {
+        data.password=await bcrypt.hash(req.body.password, 12);
+
+        const old=await userModel.find({_id: req.params.id});
+
+        if(old.length=0) {
+            return res.status(400).json({
+                status: "failed",
+                message: "User not found !"
+            })
+        }
+
+        await userModel.updateOne({_id: req.params.id}, data, {upsert: true})
+        const token=jwt.sign({rollNo: req.body.rollNo}, process.env.JWT_KEY, {expiresIn: process.env.JWT_EXPIRES})
+        res.status(200).json({
+            message: "User Updated successfully 😌",
+            token,
+            data: data
+        });
+    } catch(error) {
+        console.log(error);
+        return res.status(400).json({
+            status: "Failed",
+            error: error
+        });
+    }
 }
 
-exports.deleteUser=(req, res) =>
+exports.updateProfile=async (req, res) =>
 {
-    res.status(200).json({status: "Delete user Done"});
+    const data=req.body;
+    try {
+        const old=await userModel.find({_id: req.params.id});
+
+        if(old.length=0) {
+            return res.status(400).json({
+                status: "failed",
+                message: "User not found !"
+            })
+        }
+
+        await userModel.updateOne({_id: req.params.id}, {photo: data.photo}, {upsert: true})
+        res.status(200).json({
+            message: "User Profile Updated successfully 😌",
+            data: data
+        });
+    } catch(error) {
+        console.log(error);
+        return res.status(400).json({
+            status: "Failed",
+            error: error
+        });
+    }
+}
+
+exports.updatePassword=async (req, res) =>
+{
+    const data=req.body;
+    try {
+        data.password=await bcrypt.hash(req.body.password, 12);
+
+        const old=await userModel.find({_id: req.params.id});
+
+        if(old.length=0) {
+            return res.status(400).json({
+                status: "failed",
+                message: "User not found !"
+            })
+        }
+
+        await userModel.updateOne({_id: req.params.id}, {password: data.password}, {upsert: true})
+
+        const token=jwt.sign({rollNo: old.rollNo}, process.env.JWT_KEY, {expiresIn: process.env.JWT_EXPIRES})
+        res.status(200).json({
+            message: "Password Updated successfully 😌",
+            token: {token}
+        });
+    } catch(error) {
+        console.log(error);
+        return res.status(400).json({
+            status: "Failed",
+            error: error
+        });
+    }
+}
+
+exports.deleteUser=async (req, res) =>
+{
+    try {
+        const old=await userModel.find({_id: req.params.id});
+
+        if(old.length=0) {
+            return res.status(400).json({
+                status: "failed",
+                message: "User not found !"
+            })
+        }
+
+        const msg=await userModel.deleteOne({_id: req.params.id})
+
+        res.status(200).json({
+            message: "User deleted successfully 😌",
+            result: {msg}
+        });
+    } catch(error) {
+        console.log(error);
+        return res.status(400).json({
+            status: "Failed",
+            error: error
+        });
+    }
 }
