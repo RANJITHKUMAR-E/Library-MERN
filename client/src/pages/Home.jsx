525a2c00cafbd68881d50102dc437f11b166f13f
Mukilan-T <mukilanthangadurai@gmail.com>
2023-02-10 10:07:03 +0530

    login st

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import HomeView from '../components/HomeView';
+import Login from '../components/Login';
 
 const Home = () => {
     return (
-        <HomeView/>
+        <Login/>
     )
 }
 
