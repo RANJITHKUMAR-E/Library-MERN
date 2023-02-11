196d4600c374916f2dd83d425be746de4f8ec632
Mukilan-T <mukilanthangadurai@gmail.com>
2023-02-11 18:18:44 +0530

    Login_updated

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -8,13 +8,13 @@ export default function LoginPage() {
   return (
     <>
       <div className="row justify-content-center mt-5">
-        <div className="col-md-4 mt-5 text-center mb-5 ">
+        <div className="col-6 mt-5 text-center" >
           <h2 className="m-8" style={{   fontSize: "25px" }}>
             ONLINE LIBRARY MANAGEMENT SYSTEM
           </h2>
         </div> 
       </div>
-
+<br />
       <div className="row justify-content-center mt-5">
         <div className="col-md-5 mt-3 text-left  p-3 mb-5 ">
         <img src="https://iili.io/HEMIGwB.png" alt="..."  style={{width:"350px"}}/>
@@ -40,20 +40,20 @@ export default function LoginPage() {
               
             />
             <h5 className="m-2" style={{fontFamily: 'Inter',
-fontStyle: "normal" ,padding:"5px"}}>
+fontStyle: "normal" ,padding:"5px", fontWeight:"bold"}}>
                   Forgot Password ?{" "}
-                  <a href="/register" style={{ textDecoration: "none" }}>
+                  <a href="/register" style={{ textDecoration: "none",color:"rgba(0, 0, 0, 0.4)" }}>
                     Click Here
                   </a>
                 </h5>
-            <button className="btn mt-3" >
+            <button className="btn mt-2" >
               Login
             </button>
                 <br />
                 <h5 className="m-2" style={{fontFamily: 'Inter',
-fontStyle: "normal" ,padding:"5px"}}>
-                  New User{" "}
-                  <a href="/register" style={{ textDecoration: "none" }}>
+fontStyle: "normal" ,padding:"5px", fontWeight:"bold"}}>
+                  New User? {" "}
+                  <a href="/register" style={{ textDecoration: "none" ,color:"rgba(0, 0, 0, 0.4)"}}>
                     Create Account
                   </a>
                 </h5>
