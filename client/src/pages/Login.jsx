8ec613153ff6519054c6dce714420e99c1dd3cfc
RANJTH KUMAR <90784111+RANJITHKUMAR-E@users.noreply.github.com>
2023-03-06 18:02:07 +0530

    Host02

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
   import { ToastContainer, toast } from "react-toastify";
   import "react-toastify/dist/ReactToastify.css";
   import Alerts from "../components/Alert";
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function SignUpPage() {
   // const [userData, setData] = useState({
